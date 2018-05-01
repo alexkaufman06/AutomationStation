248fe7ced67e35a8a83da9ed66046919c2e3fcf2
alexkaufman06 <alexkaufman06@gmail.com>
2018-05-01 11:07:49 -0700

    Add initial files

---
+++ src/Tests/ApplicationTests.java
@@ -0,0 +1,12 @@
+package Tests;
+
+import org.testng.annotations.Test;
+
+public class ApplicationTests {
+  @Test
+  public void doLogin() {
+	for (int i=1;i<11;i++) {
+	  System.out.println(i + ") Hello Java!");
+	}
+  }
+}
